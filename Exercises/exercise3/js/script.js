9ef1f25a0549608ece4d995b525d7c9f34f60580
Frédérick Labbé <f_labb@cda088ev5815x.local>
2019-10-03 14:15:20 -0400

    Added more comments & put reset code in reset function

--- Exercises/exercise3/js/script.js
+++ Exercises/exercise3/js/script.js
@@ -2,11 +2,11 @@
 
 /******************************************************************************
 Where's Sausage Dog?
-by Pippin Barr
+by Frederick Labbe
 
 An algorithmic version of a Where's Wally/Waldo searching game where you
 need to click on the sausage dog you're searching for in amongst all
-the visual noise of other animals.
+the visual noise of other animals of different sizes and random positions.
 
 Animal images from:
 https://creativenerds.co.uk/freebies/80-free-wildlife-icons-the-best-ever-animal-icon-set/
@@ -16,7 +16,7 @@ https://creativenerds.co.uk/freebies/80-free-wildlife-icons-the-best-ever-animal
 let targetX;
 let targetY;
 let targetImage;
-
+//Variables for Perlin Noise fot trajectory of dog
 let tx;
 let ty;
 
@@ -60,26 +60,100 @@ function preload() {
 // setup()
 //
 // Creates the canvas, sets basic modes, draws correct number
-// of decoys in random positions, then the target
+// of decoys in random positions, then the target by calling reset
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background("#ffff00");
   imageMode(CENTER);
 
+  //Calls the reset function
+  reset();
+}
 
+// draw()
+//
+// Displays the game over screen if the player has won,
+// otherwise nothing (all the gameplay stuff is in mousePressed())
+function draw() {
+
+  if (gameOver) {
+
+    background("#ffff00");
+    // Prepare our typography
+    textFont("Helvetica");
+    textSize(128);
+    textAlign(CENTER,CENTER);
+    noStroke();
+    fill(random(255));
+
+    // Tell them they won!
+    text("YOU WON!",width/2,height/2);
+
+    // Draw a circle around the sausage dog to show where it is (even though
+    // they already know because they found it!)
+    noFill();
+    stroke(random(255));
+    strokeWeight(10);
+    ellipse(targetX,targetY,targetImage.width,targetImage.height);
+
+    //Calls the function move() to make the dog move
+    move();
+  }
+}
+
+// mousePressed()
+//
+// Checks if the player clicked on the target and if so tells them they won
+function mousePressed() {
+  // The mouse was clicked!
+  if(gameOver === true){
+    background("#ffff00");
+    imageMode(CENTER);
+    //Calls the reset to be able to play again
+    reset();
+  }
+
+
+  // Check if the cursor is in the x range of the target
+  // (We're subtracting the image's width/2 because we're using imageMode(CENTER) -
+  // the key is we want to determine the left and right edges of the image.)
+  if (mouseX > targetX - targetImage.width/2 && mouseX < targetX + targetImage.width/2) {
+    // Check if the cursor is also in the y range of the target
+    // i.e. check if it's within the top and bottom of the image
+    if (mouseY > targetY - targetImage.height/2 && mouseY < targetY + targetImage.height/2) {
+      gameOver = true;
+    }
+  }
+
+}
+//Makes the dog move according to the Perlin Noise
+function move(){
+
+
+  targetX = width * noise(tx);
+  targetY = height * noise(ty);
+  tx += 0.01;
+  ty += 0.01;
+  image(targetImage,targetX,targetY);
+}
+//Sets up or resets the game so it goes back to how it was before playing
+function reset(){
 
   // Use a for loop to draw as many decoys as we need
   for (let i = 0; i < numDecoys; i++) {
     // Choose a random location on the canvas for this decoy
     let x = random(0,width);
     let y = random(0,height);
+
     // moves the image if it's under the rectangle
     if(x>(width-350) && y<(170)){
       x = 100 + random(0, 100);
       y = 200 + random(0, 100);
     }
+
     //random size of images
     let randSize = random(70,200);
+
     // Generate a random number we can use for probability
     let r = random();
     // Use the random number to display one of the ten decoy
@@ -121,6 +195,7 @@ function setup() {
   //sets up the rectangle
   rectMode(CORNER);
   fill(255);
+  noStroke();
   rect(windowWidth-300,0,300,150);
   // Once we've displayed all decoys, we choose a random location for the target
   targetX = random(0,width);
@@ -145,156 +220,7 @@ function setup() {
   //random movement of dog
   tx = random(0,100);
   ty = random(0,100);
-}
 
-
-// draw()
-//
-// Displays the game over screen if the player has won,
-// otherwise nothing (all the gameplay stuff is in mousePressed())
-function draw() {
-
-  // //sets up the rectangle
-  // fill(255);
-  // rect(windowWidth,0,200,100);
-
-  if (gameOver) {
-
-    background("#ffff00");
-    // Prepare our typography
-    textFont("Helvetica");
-    textSize(128);
-    textAlign(CENTER,CENTER);
-    noStroke();
-    fill(random(255));
-
-    // Tell them they won!
-    text("YOU WINNED!",width/2,height/2);
-
-    // Draw a circle around the sausage dog to show where it is (even though
-    // they already know because they found it!)
-    noFill();
-    stroke(random(255));
-    strokeWeight(10);
-    ellipse(targetX,targetY,targetImage.width,targetImage.height);
-
-    move();
-  }
-}
-
-// mousePressed()
-//
-// Checks if the player clicked on the target and if so tells them they won
-function mousePressed() {
-  // The mouse was clicked!
-  if(gameOver === true){
-    background("#ffff00");
-    imageMode(CENTER);
-
-
-
-    // Use a for loop to draw as many decoys as we need
-    for (let i = 0; i < numDecoys; i++) {
-      // Choose a random location on the canvas for this decoy
-      let x = random(0,width);
-      let y = random(0,height);
-      // moves the image if it's under the rectangle
-      if(x>(width-350) && y<(170)){
-        x = 100 + random(0, 100);
-        y = 200 + random(0, 100);
-      }
-      //random size of images
-      let randSize = random(70,200);
-      // Generate a random number we can use for probability
-      let r = random();
-      // Use the random number to display one of the ten decoy
-      // images, each with a 10% chance of being shown
-      // We'll talk more about this nice quality of random soon enough.
-      // But basically each "if" and "else if" has a 10% chance of being true
-      if (r < 0.1) {
-        image(decoyImage1,x,y,randSize,randSize);
-      }
-      else if (r < 0.2) {
-        image(decoyImage2,x,y,randSize,randSize);
-      }
-      else if (r < 0.3) {
-        image(decoyImage3,x,y,randSize,randSize);
-      }
-      else if (r < 0.4) {
-        image(decoyImage4,x,y,randSize,randSize);
-      }
-      else if (r < 0.5) {
-        image(decoyImage5,x,y,randSize,randSize);
-      }
-      else if (r < 0.6) {
-        image(decoyImage6,x,y,randSize,randSize);
-      }
-      else if (r < 0.7) {
-        image(decoyImage7,x,y,randSize,randSize);
-      }
-      else if (r < 0.8) {
-        image(decoyImage8,x,y,randSize,randSize);
-      }
-      else if (r < 0.9) {
-        image(decoyImage9,x,y,randSize,randSize);
-      }
-      else if (r < 1.0) {
-        image(decoyImage10,x,y,randSize,randSize);
-      }
-    }
-
-    //sets up the rectangle
-    rectMode(CORNER);
-    fill(255);
-    rect(windowWidth-300,0,300,150);
-    // Once we've displayed all decoys, we choose a random location for the target
-    targetX = random(0,width);
-    targetY = random(0,height);
-
-    // moves the target dog if it's under/top of the rectangle
-    if(targetX>(width-350) && targetY<(170)){
-      targetX = 100 + random(0, 200);
-      targetY = 200 + random(0, 200);
-    }
-
-    // And draw it (because it's the last thing drawn, it will always be on top)
-    image(targetImage,targetX,targetY);
-    image(targetImage,(width -160),50);
-
-    //Displaying text
-    fill(0);
-    textFont("Helvetica");
-    textSize(30);
-    text("Chien Perdu",(width-250), 120);
-
-    //random movement of dog
-    tx = random(0,100);
-    ty = random(0,100);
-    gameOver = false;
-  }
-
-
-  // Check if the cursor is in the x range of the target
-  // (We're subtracting the image's width/2 because we're using imageMode(CENTER) -
-  // the key is we want to determine the left and right edges of the image.)
-  if (mouseX > targetX - targetImage.width/2 && mouseX < targetX + targetImage.width/2) {
-    // Check if the cursor is also in the y range of the target
-    // i.e. check if it's within the top and bottom of the image
-    if (mouseY > targetY - targetImage.height/2 && mouseY < targetY + targetImage.height/2) {
-      gameOver = true;
-    }
-  }
-
-}
-function move(){
-
-
-  targetX = width * noise(tx);
-  targetY = height * noise(ty);
-  tx += 0.01;
-  ty += 0.01;
-  image(targetImage,targetX,targetY);
-}
-function reset(){
-
+  //sets back the boolean gameOver to false because the game is restarting
+  gameOver = false;
 }
